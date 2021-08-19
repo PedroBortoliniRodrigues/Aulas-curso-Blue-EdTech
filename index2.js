defdf54902e474886a5aa0b6caa5e3093455a700
Pedro Henrique Bortolini Rodrigues <89049144+PedroBortoliniRodrigues@users.noreply.github.com>
2021-08-19 18:36:02 -0400

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,8 @@
+//Exercício 4 - Elabore um programa que recebe dois valores inteiros e mostra a soma desses valores
+let numero1 = parseInt (prompt  ("digite um numero:"))
+console.log (numero1)
+let numero2 = parseInt (prompt ("digite um numero:"))
+console.log (numero2)
+soma = (numero1) +(numero2)
+
+console.log(soma)
