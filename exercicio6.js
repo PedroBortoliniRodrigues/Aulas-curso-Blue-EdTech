691cb57113860d2718f66d011c4276a5ca1d87d4
Pedro Henrique Bortolini Rodrigues <89049144+PedroBortoliniRodrigues@users.noreply.github.com>
2021-08-19 18:35:38 -0400

    Add files via upload

---
+++ exercicio6.js
@@ -0,0 +1,7 @@
+//Exercício 6 - Elabore um programa que recebe dois valores inteiros e mostra se o primeiro valor é maior ou igual ao segundo valor
+let numero1 = parseInt (prompt ("digite um numero:"))
+let numero2 = parseInt (prompt ("digite um numero:"))
+
+valor = numero1 >= numero2
+
+console.log(valor)
