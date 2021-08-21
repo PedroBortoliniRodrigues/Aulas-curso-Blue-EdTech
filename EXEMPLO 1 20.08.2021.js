682816702a05970842cb4796340ae88f5cd15f22
Pedro Henrique Bortolini Rodrigues <89049144+PedroBortoliniRodrigues@users.noreply.github.com>
2021-08-20 21:40:03 -0300

    Add files via upload

---
+++ EXEMPLO 1 20.08.2021.js
@@ -0,0 +1,13 @@
+//Faça um programa que pergunte ao usuário um número e valide se é par ou ímpar:
+// Crie uma variável para receber o valor, com conversão para int
+// Para um número ser par, a divisão dele por 2 tem que dar resto 0
+
+
+  let numero = prompt("Digite um numero:");
+
+    if (numero % 2 === 0){ 
+      console.log('Seu numero é par');
+      } else { 
+        console.log("Seu numero impar");
+      }
+        
